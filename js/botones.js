21853c5398a80d613a1a2aaae114f201895c98ef
mg.seguridad.cba <mg.seguridad.cba@gmail.com>
2024-06-12 21:19:01 +0000

    Cmoniexo de botones

---
+++ js/botones.js
@@ -0,0 +1,21 @@
+switch (name_boton) {
+    case 'inicio':
+      // Code to execute if expression === value1
+      break;
+    case 'proyectos':
+      // Code to execute if expression === value2
+      break;
+    // ... more cases
+    case 'prototipos':
+        // Code to execute if expression === value2
+      break;
+    case 'embebidos':
+      // Code to execute if expression === value2
+      break;
+    case 'acerca':
+        alert("presionasteboton de acerca");
+        break;
+      default:
+      // Code to execute if no other case matches
+  }
+  
